a57b98757e7624dbdec5acf998e6cfbc4e8d8bdb
Ex05 <ex05@web.de>
2016-06-19 12:49:53 +0200

    Added propertyFile for server settings.

--- Server/src/main/java/de/presidente/Main.java
+++ Server/src/main/java/de/presidente/Main.java
@@ -3,7 +3,11 @@ package de.presidente;
 
 // <- Static_Import ->
 
+import de.janik.propertyFile.PropertyFile;
+import de.janik.propertyFile.exception.NoSuchEntryException;
 import de.presidente.server.Server;
+
+import java.io.IOException;
 
 /**
  * @author Jan.Marcel.Janik [©2016]
@@ -15,7 +19,7 @@ public final class Main {
     // <- Static ->
 
     // <- Constructor ->
-    private Main(){
+    private Main() {
         throw new IllegalStateException("Do not instantiate.");
     }
 
@@ -24,8 +28,26 @@ public final class Main {
     // <- Getter & Setter ->
 
     // <- Static ->
-    public static void main(final String[] args){
-        final Server server = new Server(5585);
-        server.start();
+    public static void main(final String[] args) {
+
+        PropertyFile properties;
+        try {
+            properties = PropertyFile.CreateNewFile("./res/options.properties");
+
+            final String propertyServerPort = "server_port";
+
+            if (!properties.contains(propertyServerPort)) {
+                properties.addIntProperty(propertyServerPort);
+
+                properties.getProperty(propertyServerPort).asInt().set(5585);
+            }
+
+            final int port = properties.getProperty(propertyServerPort).asInt().get();
+
+            final Server server = new Server(port);
+            server.start();
+        } catch (final IOException | NoSuchEntryException e) {
+            e.printStackTrace();
+        }
     }
 }
