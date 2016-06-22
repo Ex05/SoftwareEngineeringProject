4284da3660cf562880f48fb4fced72ec97986547
Ex05 <ex05@web.de>
2016-06-22 13:27:31 +0200

    Added login handling.

---
+++ Network/src/main/java/de/presidente/net/Packet_009_UsernameAvailable.java
@@ -0,0 +1,24 @@
+package de.presidente.net;
+// <- Import ->
+
+// <- Static_Import ->
+
+/**
+ * @author Jan.Marcel.Janik [©2016]
+ */
+public final class Packet_009_UsernameAvailable extends Packet_003_Permission {
+    // <- Public ->
+    // <- Protected ->
+    // <- Private->
+    // <- Static ->
+
+    // <- Constructor ->
+    public Packet_009_UsernameAvailable(final Permission permission) {
+        super(permission);
+    }
+
+    // <- Abstract ->
+    // <- Object ->
+    // <- Getter & Setter ->
+    // <- Static ->
+}
